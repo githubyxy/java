cd357465046a2313010ba1f652f2e55ccf95bbe4
于晓宇 <yuxiaoyu@yuxiaoyu.local>
2017-10-11 08:56:48 +0800

    设计模式

---
+++ java/src/test/java/designpattern/builder/Builder.java
@@ -0,0 +1,8 @@
+package designpattern.builder;
+
+public abstract class Builder {
+
+	public abstract void builderPartA();
+	public abstract void builderPartB();
+	public abstract Product getResult();
+}
