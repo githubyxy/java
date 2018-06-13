8c6f94f623f1164374549da3667b7f3bae2ee6ca
于晓宇 <yuxiaoyu@yuxiaoyu.local>
2018-06-13 16:46:02 +0800

    多线程编程核心技术一二章

---
+++ java/src/test/java/innerClass/Main.java
@@ -0,0 +1,22 @@
+package innerClass;
+
+import innerClass.MemberInnerClass.Draw;
+import innerClass.StaticInnerClass.Inner;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		MemberInnerClass a = new MemberInnerClass(1);
+//		System.out.println(a.count);
+//		
+//		Draw draw = a.new Draw();
+////		Draw draw = a.getDrawInstance();
+//		draw.drawSahpe();
+		
+		
+		StaticInnerClass b = new StaticInnerClass();
+//		System.out.println(b.a);
+		
+		Inner inner = new StaticInnerClass.Inner();
+	}
+}
---
+++ java/src/test/java/multithreading/synchronizedDemo/MyDomain9.java
@@ -0,0 +1,27 @@
+package multithreading.synchronizedDemo;
+
+/**
+ * synchronized同步方法 与 synchronized(this)同步代码块
+ * @author yuxiaoyu
+ *
+ */
+public class MyDomain9 {
+
+	public void serviceMethodA() {
+		synchronized (this) {
+			try {
+				System.out.println("A begin time = " + System.currentTimeMillis());
+				Thread.sleep(2000);
+				System.out.println("A end time = " + System.currentTimeMillis());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	synchronized public void serviceMethodB() {
+		System.out.println("B begin time = " + System.currentTimeMillis());
+		System.out.println("B end time = " + System.currentTimeMillis());
+	}
+}
